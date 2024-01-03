74e2a1ee110734442bc918354162b4721ffa17f0
Sumin <new.sumyang@gmail.com>
2024-01-03 18:40:57 +0900

    add

---
+++ src/main/java/hello/winter/repository/DataRepository.java
@@ -0,0 +1,21 @@
+package hello.winter.repository;
+
+import hello.winter.domain.DTO;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataRepository {
+    List<DTO> dataList = new ArrayList<>();
+
+    public List<DTO> findCol(ResultSet rs,String col) throws SQLException {
+        while (rs.next()) {
+            DTO dto = new DTO();
+            dto.setCal(rs.getInt("CALENDAR"));
+            dto.setPrice(rs.getInt(col));
+            dataList.add(dto);
+        }
+        return dataList;
+    }
+}
